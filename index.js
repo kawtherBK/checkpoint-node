531bcb1855088a49413c5daff24a65983e0f149b
kawther boukamcha <boukamchakawther@gmail.com>
2022-06-28 18:03:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// const fs = require('fs');
+
+// fs.readFile('/text.txt', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// console.log("HELLO WORLD")
+// var result = 0;
+// for (var i = 2; i < process.argv.length; i++)
+//   result += Number(process.argv[i])
+// console.log(result)
+
+
